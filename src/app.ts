2ce9543ebf1b68809be2abb63d8a98e88ed2b09a
Davi Costa de Melo <davicosta01082008@gmail.com>
2023-03-12 13:11:03 -0300

    Config CORS

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import fastify from 'fastify'
 import { tasksRoutes } from './routes/tasks'
 import cookie from '@fastify/cookie'
+import cors from '@fastify/cors'
 
 export const app = fastify()
 
 app.register(cookie)
+app.register(cors)
 app.register(tasksRoutes, {
   prefix: 'tasks',
 })
